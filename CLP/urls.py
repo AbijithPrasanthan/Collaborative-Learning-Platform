87399030fd8f8b37b40b7ea881e12f5c7f94d35c
Restyled.io <commits@restyled.io>
2022-01-18 12:38:23 +0000

    Restyled by black

--- CLP/urls.py
+++ CLP/urls.py
@@ -4,7 +4,7 @@ from CLP import views
 
 
 urlpatterns = [
-    path('login/', views.register, name='login'),
-    path('home/', views.login_, name='home'),
-    path('login/', views.user_logout, name='user_logout'),
+    path("login/", views.register, name="login"),
+    path("home/", views.login_, name="home"),
+    path("login/", views.user_logout, name="user_logout"),
 ]
--- CLP/views.py
+++ CLP/views.py
@@ -6,42 +6,41 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib import messages
+
 # Create your views here.
 
 
 def index(request):
-    d = {
-        'insert_me': 'This is the view.py file'
-    }
-    return render(request, 'CLP/index.html', context=d)
+    d = {"insert_me": "This is the view.py file"}
+    return render(request, "CLP/index.html", context=d)
 
 
 def login_(request):
-    return render(request, 'CLP/home.html')
+    return render(request, "CLP/home.html")
 
 
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('login'))
+    return HttpResponseRedirect(reverse("login"))
 
 
 def register(request):
-    if request.method == 'POST' and 'signin' in request.POST:
-        username = request.POST.get('Username')
-        password = request.POST.get('Password')
+    if request.method == "POST" and "signin" in request.POST:
+        username = request.POST.get("Username")
+        password = request.POST.get("Password")
         user = authenticate(username=username, password=password)
         if user:
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect(reverse('home'))
+                return HttpResponseRedirect(reverse("home"))
             else:
-                return HttpResponse('ACCOUNT NOT ACTIVE')
+                return HttpResponse("ACCOUNT NOT ACTIVE")
         else:
             print("SOMEONE TRIED TO LOGIN AND FAILED")
             print("Username: {} and password: {}".format(username, password))
-            return redirect('login')
-    elif request.method == 'POST' and 'signup' in request.POST:
+            return redirect("login")
+    elif request.method == "POST" and "signup" in request.POST:
         registered = False
         profile_form = UserProfileInfoForm(data=request.POST)
         user_form = UserForm(data=request.POST)
@@ -60,4 +59,8 @@ def register(request):
     else:
         profile_form = UserProfileInfoForm()
         user_form = UserForm()
-    return render(request, 'CLP/login.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(
+        request,
+        "CLP/login.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
