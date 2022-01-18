af396306dc402b41adfb16b8ce1c7dea61391b02
rahanmanoj <56351775+rahanmanoj@users.noreply.github.com>
2022-01-18 18:03:07 +0530

    Added login and logout

--- CLP/urls.py
+++ CLP/urls.py
@@ -4,6 +4,7 @@ from CLP import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('login/', views.register, name='login'),
+    path('home/', views.login_, name='home'),
+    path('login/', views.user_logout, name='user_logout'),
 ]
--- CLP/views.py
+++ CLP/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from CLP.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.contrib import messages
 # Create your views here.
 
 
@@ -11,13 +16,32 @@ def index(request):
     return render(request, 'CLP/index.html', context=d)
 
 
-def login(request):
-    return render(request, 'CLP/login.html')
+def login_(request):
+    return render(request, 'CLP/home.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
 
 
 def register(request):
-    registered = False
-    if(request.method == 'POST'):
+    if request.method == 'POST' and 'signin' in request.POST:
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('ACCOUNT NOT ACTIVE')
+        else:
+            print("SOMEONE TRIED TO LOGIN AND FAILED")
+            print("Username: {} and password: {}".format(username,password))
+            return redirect('login')
+    elif request.method == 'POST' and 'signup' in request.POST:
+        registered = False
         profile_form = UserProfileInfoForm(data=request.POST)
         user_form = UserForm(data=request.POST)
 
@@ -35,5 +59,4 @@ def register(request):
     else:
         profile_form = UserProfileInfoForm()
         user_form = UserForm()
-
     return render(request, 'CLP/login.html', {'user_form': user_form, 'profile_form': profile_form})
