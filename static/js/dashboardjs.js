2b84f81e18415793f7c22bf0b4b4bd535bc92c9b
bharathprathap <iambharath11@gmail.com>
2022-02-08 03:16:03 +0530

    Added pop-up window for the cards.But js script for it not working..Will look into it later :)

--- static/js/dashboardjs.js
+++ static/js/dashboardjs.js
@@ -1,2 +1,17 @@
 feather.replace()
 //https://twitter.com/One_div
+
+var modal = document.getElementById("modal");
+// Get the card that opens the modal
+var card = document.getElementById("card");
+var closebtn = document.getElementById("close");
+
+// When the user clicks the card, open the modal 
+card.onclick = function() {
+    modal.style.display = "block";
+  }
+
+// When the user clicks on  (x), close the modal
+closebtn.onclick = function() {
+    modal.style.display = "none";
+  }
