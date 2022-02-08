619765788ff9537119b99107d4a2839ef8484499
bharathprathap <iambharath11@gmail.com>
2022-02-08 23:15:28 +0530

    corrected issues with pop-up window

--- static/js/dashboardjs.js
+++ static/js/dashboardjs.js
@@ -1,17 +1,29 @@
 feather.replace()
 //https://twitter.com/One_div
 
+//Get the popup window(modal)
 var modal = document.getElementById("modal");
-// Get the card that opens the modal
-var card = document.getElementById("card");
+
+// Get the card that opens the modal and store it as an array cards
+cards=document.querySelectorAll(".card");
+
+//Get the close button
 var closebtn = document.getElementById("close");
 
-// When the user clicks the card, open the modal 
-card.onclick = function() {
+var overlay=document.getElementById("overlay  ");
+
+for (let i = 0; i < cards.length; i++) 
+{
+  // When the user clicks the card, open the modal 
+  cards[i].onclick = function() 
+  {
     modal.style.display = "block";
+    // overlay.classList.add('active')
   }
+}
 
 // When the user clicks on  (x), close the modal
-closebtn.onclick = function() {
+closebtn.onclick = function() 
+{
     modal.style.display = "none";
-  }
+}
